34ecc9f9116061b68e94ee57ea682f2a3ef0d26c
Yuditskiy-o <Komekadze@gmail.com>
2020-07-18 22:22:25 +0700

    CreditPaymentService added

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,15 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class CreditPaymentService {
+    public BigDecimal calculate (int term, int credit) {
+        BigDecimal rate = new BigDecimal(9.99/100/12);
+        BigDecimal a = new BigDecimal(1).add(rate);
+        int b = term * 12;
+        BigDecimal c = a.pow(b);
+        BigDecimal e = new BigDecimal(1).divide(c,10000,RoundingMode.UP);
+        BigDecimal d = new BigDecimal(1).subtract(e);
+        BigDecimal payment = new BigDecimal(credit).multiply(rate).divide(d,0,RoundingMode.UP);
+        return payment;
+    }
+}
